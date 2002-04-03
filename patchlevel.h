f9f9c009f568d8a029665fc02b8775f4176bbc0e
rmg <a@b>
2002-04-03 19:48:08 +0100

    Branding 2.4rc2
    [git-p4: depot-paths = "//guest/perforce_software/jam/src/": change = 1617]

--- patchlevel.h
+++ patchlevel.h
@@ -1,5 +1,5 @@
 /* Keep JAMVERSYM in sync with VERSION. */
 /* It can be accessed as $(JAMVERSION) in the Jamfile. */
 
-#define VERSION "2.4rc1"
+#define VERSION "2.4rc2"
 #define JAMVERSYM "JAMVERSION=2.4"
