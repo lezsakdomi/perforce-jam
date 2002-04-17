1909d21dfae1f232f19e9b6017c8f3b72f68fc9b
rmg <a@b>
2002-04-17 20:21:02 +0100

    2.4rc2 becomes 2.4 (final).
    [git-p4: depot-paths = "//guest/perforce_software/jam/src/": change = 1658]

--- patchlevel.h
+++ patchlevel.h
@@ -1,5 +1,5 @@
 /* Keep JAMVERSYM in sync with VERSION. */
 /* It can be accessed as $(JAMVERSION) in the Jamfile. */
 
-#define VERSION "2.4rc2"
+#define VERSION "2.4"
 #define JAMVERSYM "JAMVERSION=2.4"
