97b518aad259e46b205c433790bd6371ba7a9670
rmg <a@b>
2002-12-15 22:07:30 +0100

    Fix SubInclude's error message so that it actually mentions the variable that is unset.
    Also bumped patchlevel to 2.4.2 capriciously.
    
    Change to unreleased behavior.
    
    === computer:1666: Change 38182 by seiwald@tricks-seiwald on 2002/11/25 10:47:50
    
    [git-p4: depot-paths = "//guest/perforce_software/jam/src/": change = 2496]

--- patchlevel.h
+++ patchlevel.h
@@ -1,5 +1,5 @@
 /* Keep JAMVERSYM in sync with VERSION. */
 /* It can be accessed as $(JAMVERSION) in the Jamfile. */
 
-#define VERSION "2.4.1"
-#define JAMVERSYM "JAMVERSION=2.4.1"
+#define VERSION "2.4.2"
+#define JAMVERSYM "JAMVERSION=2.4.2"
